1635275178220a5144c68c6ecfa480e09abca5fb
shreyasapaliga312 <120451969+shreyasapaliga312@users.noreply.github.com>
2025-08-25 18:47:22 +0530

    Add files via upload

---
+++ 05.py
@@ -0,0 +1,24 @@
+# text = "Welcome to Python!"
+# print(text[:7]) # Output: Welcome
+# print(text[-7:]) # Output: Python!
+# print(text[3:-3]) # Output: come to Pyt
+
+# name=input("Enter your name:")
+# age=int(input("Enter your age:"))
+
+# print(f"My name is {name} and I am {age} years old")
+# text ="Python"
+# print(f"{text:>10}") # Right align
+# print(f"{text:<10}") # Left align
+# print(f"{text:^10}") # Center align
+
+# def greet(name):
+#      return f"Hello, {name}"
+# a=greet("shreya")
+# print(a)
+
+# square=lambda x:x*x
+# print(square(4))
+
+import math
+print(math.sqrt(16))
