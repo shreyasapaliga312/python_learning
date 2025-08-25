f7c608f63e6fbccb27b1d9d8916e7ca8b8604d75
shreyasapaliga312 <120451969+shreyasapaliga312@users.noreply.github.com>
2025-08-25 18:46:31 +0530

    Add files via upload

---
+++ 04.py
@@ -0,0 +1,9 @@
+age=int(input("Enter your age:"))
+
+if(age<18):
+    print("You cannot drive")
+elif(age==18):
+    print("you just turned 18.You can drive")
+else:
+    print("You can drive")
+print("ooooooooooooh!")
