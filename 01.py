992e367ef8bb522263a63f9a87539f8d3b4df681
shreyasapaliga312 <120451969+shreyasapaliga312@users.noreply.github.com>
2025-08-25 18:43:04 +0530

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,15 @@
+print("Hello world")
+
+name="shreya sapaliga"
+age=21
+cgpa=9.02
+is_student=True
+
+print(name)
+print(age)
+print(cgpa)
+print(is_student)
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(is_student))
