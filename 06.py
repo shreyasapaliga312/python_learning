42474f5dafbe53291d09710b55cfd3933bbe927b
shreyasapaliga312 <120451969+shreyasapaliga312@users.noreply.github.com>
2025-08-25 18:49:45 +0530

    Add files via upload

---
+++ 06.py
@@ -0,0 +1,25 @@
+# def greet(name):
+#     return f"Hello {name}"
+
+
+# print(greet("shreya"))
+
+# def add(a,b):
+#     return a+b
+
+# print(add(5,4))
+
+
+# def greet(age,name="shreya"):
+#     return f"Hello, {name},{age}"
+
+# print(greet("shr"))
+
+# def factorial(n):
+#     if n==1:
+#         return 1
+#     return n*factorial(n-1)
+# print(factorial(5))
+
+square=lambda x,y:x*y
+print(square(4,5))
