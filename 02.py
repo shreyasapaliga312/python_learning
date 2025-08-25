e2b596f2dee2e16833863a7f8c48018f52775d13
shreyasapaliga312 <120451969+shreyasapaliga312@users.noreply.github.com>
2025-08-25 18:44:03 +0530

    Add files via upload

---
+++ 02.py
@@ -0,0 +1,14 @@
+num_str="10"
+num_int=int(num_str)
+print(num_int)
+print(type(num_int))
+
+num=25
+num_str2=str(num)
+print(num_str2)
+print(type(num_str2))
+
+pi=3.14
+pi_int=int(pi)
+print(pi_int)
+print(type(pi_int))
